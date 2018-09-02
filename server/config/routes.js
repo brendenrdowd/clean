1f2fe6aea8348b5fbbfe14d4c57653264c8b1ce2
brendenrdowd <brenden.dowd@yahoo.com>
2018-09-01 22:21:43 -0700

    routing

--- server/config/routes.js
+++ server/config/routes.js
@@ -4,6 +4,7 @@ const path = require('path');
 module.exports = function(app){
     //log-reg functions
     app.post('/register', (req,res)=>{
+        console.log("hitting register route")
         Prime.register(req,res);
     })
     app.post('/login', (req,res)=>{
